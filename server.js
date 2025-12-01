42f8c16f9ae7d90b7ed8073a0493c3e37b964092
Abhinav909-jpg <abhinavsharma7686@gmail.com>
2025-12-01 12:32:33 +0530

    Restructure project: Move static files to public/ and simplify Vercel config

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ async function getDb() {
 
 // connectDB(); // Removed immediate call
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
